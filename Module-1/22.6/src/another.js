016a1caca2dfffc52d4d9d88891b13cdc0de833e
Kakon Debnath <kakondebnath96@gmail.com>
2024-01-09 18:17:58 +0600

    add babel plaguin for rollup

--- Module-1/22.6/src/another.js
+++ Module-1/22.6/src/another.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const HelloTest = () => {
   return React.createElement("h2", {}, "Hello h2");
 };
--- Module-1/22.6/src/main.js
+++ Module-1/22.6/src/main.js
@@ -1,3 +1,4 @@
+// rollup main.js --file bundle.js --format esm
 import React from "react";
 import ReactDOM from "react-dom/client";
 
@@ -5,17 +6,16 @@ import { HelloTest, HelloTest2 } from "./another";
 import { Test, Test2 } from "./test";
 const App = () => {
   return React.createElement(
-    "h1",
-    {},
-    "My Manual Bundler with rollUp",
+    "div",
+    { style: { border: "1px solid black" } },
+    "",
     HelloTest(),
     HelloTest2(),
     Test(),
     Test2()
   );
 };
-// rollup main.js --file bundle.js --format esm
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(React.createElement(App));
+root.render(<App />);
