683895c9b1a0137dd5e4ef96b359c0005e49e15e
Kakon Debnath <kakondebnath96@gmail.com>
2024-01-10 13:21:24 +0600

    add data for indivisual state

--- Module-2/src/App.tsx
+++ Module-2/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import UseStateEx from "./pages/UseStateEx";
+import FormEx from "./pages/FormEx";
 
 function App() {
   const [counter, setCounter] = useState(0);
@@ -7,6 +8,7 @@ function App() {
     <div>
       <h1 className="text-center text-3xl">React App</h1>
       <UseStateEx counter={counter} setCounter={setCounter} />
+      <FormEx />
     </div>
   );
 }
