a2daa495d61e33893973a65a1a125406fdc0a466
Kakon Debnath <kakondebnath96@gmail.com>
2024-01-10 12:05:08 +0600

    add decrement fn modify

--- Module-2/src/pages/UseStateEx.tsx
+++ Module-2/src/pages/UseStateEx.tsx
@@ -3,21 +3,35 @@ import { useState } from "react";
 const UseStateEx = () => {
   console.log("click");
   const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+  const handleDecrement = () => {
+    setCounter((prevCounter) => {
+      if (prevCounter === 0) {
+        prevCounter === 0;
+      } else {
+        prevCounter -= 1;
+      }
+      return prevCounter;
+    });
+  };
+
   return (
     <div>
       <h1 className="text-red-500 font-bold text-2xl text-center mt-5">
         Counter: {counter}
       </h1>
-      <div className="flex justify-center items-center gap-4 mt-5">
-        <button
-          className="btn btn-blue"
-          onClick={() => setCounter((prevCounter) => prevCounter + 1)}
-        >
+      <div className="flex justify-center items-center gap-4 mt-5 disabled:">
+        <button className="btn btn-blue" onClick={handleIncrement}>
           Increment
         </button>
         <button
-          className="btn btn-red"
-          onClick={() => setCounter((prevCounter) => prevCounter - 1)}
+          className={`btn btn-red ${
+            counter === 0 ? "disabled:opacity-50 cursor-not-allowed" : ""
+          }`}
+          onClick={handleDecrement}
         >
           Decrement
         </button>
