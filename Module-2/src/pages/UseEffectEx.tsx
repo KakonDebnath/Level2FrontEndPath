1962e58275a853e0f5d561061c883ba4b6ba0a4a
Kakon Debnath <kakondebnath96@gmail.com>
2024-01-10 20:47:58 +0600

    useEffect

--- Module-2/src/pages/UseEffectEx.tsx
+++ Module-2/src/pages/UseEffectEx.tsx
@@ -1,24 +1,58 @@
 import { useEffect, useState } from "react";
 
 const UseEffectEx = () => {
-  const [hidden, setHidden] = useState(false);
-  useEffect(() => {
-    console.log("Render");
+  const [hidden, setHidden] = useState(true);
+  const [count, setCount] = useState(0);
+  //   useEffect(() => {
+  //     // console.log("Render");
+  //     // return () => {
+  //     //   console.log("Inside Cleanup");
+  //     // };
+  //     const intervalId = setInterval(() => {
+  //       setCount((prev) => prev + 1);
+  //       console.log(count);
+  //     }, 1000);
 
-    return () => {
-      console.log("Inside Cleanup");
-    };
-  }, [hidden]);
+  //     return () => {
+  //       clearInterval(intervalId);
+  //     };
+  //   }, []);
   return (
+    // <div>
+    //   <h1>{count}</h1>
+    //   {/* <button
+    //     onClick={() => setHidden((prev) => !prev)}
+    //     className="btn btn-blue"
+    //   >
+    //     Click
+    //   </button> */}
+    // </div>
     <div>
       <button
         onClick={() => setHidden((prev) => !prev)}
-        className="btn btn-blue"
+        className="btn btn-red"
       >
-        Click
+        {hidden ? "Show" : "Hide"}
       </button>
+      {!hidden && <Todo />}
     </div>
   );
+};
+
+const Todo = () => {
+  const abortController = new AbortController();
+  const signal = abortController.signal;
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1", { signal })
+      .then((res) => res.json())
+      .then((data) => alert(data.name));
+
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  return <div className="border border-red-600 p-10">Todo</div>;
 };
 
 export default UseEffectEx;
