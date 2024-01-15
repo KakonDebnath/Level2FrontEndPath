be26952465db2b175762a40871da8f19c7b74b72
kakon debnath <kakondebnath96@gmail.com>
2024-01-15 13:30:36 +0600

    add some to forms

--- Module-4/react-reuseable-component-module-4/src/components/ui/NormalForm/NormalForm.tsx
+++ Module-4/react-reuseable-component-module-4/src/components/ui/NormalForm/NormalForm.tsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import cn from "../../../utils/cn";
+import Button from "../Button/Button";
 
 const onSubmit = (data) => {
   console.log(data);
@@ -11,19 +12,15 @@ const NormalForm = () => {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className={cn("border border-red-500 p-5 w-full mx-auto", {
+      className={cn("border border-gray-500 rounded-md p-5 w-full mx-auto", {
         "max-w-5xl": double,
         "max-w-md": !double,
       })}
     >
       <div
-        className={cn(
-          "border border-blue-500 p-3 grid justify-items-center gap-5",
-          {
-            "grid-cols-2": double,
-            "grid-cols-1": !double,
-          }
-        )}
+        className={cn(" p-3 grid grid-cols-1 justify-items-center gap-5", {
+          "md:grid-cols-2": double,
+        })}
       >
         <div className="w-full max-w-md">
           <label htmlFor="name" className="block">
@@ -47,7 +44,7 @@ const NormalForm = () => {
           <label htmlFor="district" className="block">
             District:
           </label>
-          <select name="district" id="district">
+          <select id="district" {...register("district")}>
             <option value="">Select One</option>
             <option value="barisal">Barisal</option>
             <option value="dhaka">Dhaka</option>
@@ -59,16 +56,24 @@ const NormalForm = () => {
           <label htmlFor="address" className="block">
             Address:
           </label>
-          <textarea name="address" id="address" ></textarea>
+          <textarea id="address" {...register("address")}></textarea>
         </div>
         <div className="w-full max-w-md">
-          <label htmlFor="skill" className="block">
+          <label htmlFor="" className="block">
             Skills:
           </label>
-          <input type="checkbox" name="skill" id="skill" /> 
+          <input type="checkbox" id="html" {...register("html")} /> HTML
+          <input type="checkbox" id="css" {...register("css")} /> CSS
+          <input type="checkbox" id="js" {...register("js")} /> JS
+          <input type="checkbox" id="ts" {...register("ts")} /> TS
+          <input type="checkbox" id="react" {...register("react")} /> REACT
+        </div>
+        <div className={cn("flex justify-end w-full", {
+          "md:col-start-2": double
+        })}>
+          <Button type="submit">Submit</Button>
         </div>
       </div>
-      <button type="submit">Submit</button>
     </form>
   );
 };
