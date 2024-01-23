0e49ec5b3615c3624fac4367c0048afc8dcd9d2b
Kakon Debnath <kakondebnath96@gmail.com>
2024-01-23 22:57:31 +0600

    add navbar

--- Module-8/src/App.tsx
+++ Module-8/src/App.tsx
@@ -1,11 +1,12 @@
 // import './App.css'
 
-import { Button } from "./components/ui/button";
+import MainLayout from "./components/layouts/MainLayout";
+
 
 function App() {
   return (
     <div>
-      <Button>Click me</Button>
+      <MainLayout></MainLayout>
     </div>
   );
 }
