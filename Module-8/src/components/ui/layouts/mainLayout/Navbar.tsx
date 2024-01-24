b8246bfc58b496be264176618f0e86f833b6dd33
Kakon Debnath <kakondebnath96@gmail.com>
2024-01-25 00:20:02 +0600

    hero section animation done

--- Module-8/src/components/ui/layouts/mainLayout/Navbar.tsx
+++ Module-8/src/components/ui/layouts/mainLayout/Navbar.tsx
@@ -1,17 +1,27 @@
 import Container from "@/components/container/Container";
 import { NavLink } from "react-router-dom";
 import { Button } from "../../button";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <Container className="flex justify-between items-center py-4">
-      <h2 className="text-3xl">iRepair</h2>
-      <div className="space-x-5">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
-        <NavLink to="/">Service</NavLink>
-        <Button>Login</Button>
-      </div>
+    <Container>
+      <motion.header
+        initial={{ opacity: 0, x: -500 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, delay: 1, type: "spring", bounce: 0.5 }}
+        className="flex justify-between items-center py-4"
+      >
+        <h2 className="text-3xl">
+          <NavLink to="/">iRepair</NavLink>
+        </h2>
+        <div className="space-x-5">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+          <NavLink to="/">Service</NavLink>
+          <Button>Login</Button>
+        </div>
+      </motion.header>
     </Container>
   );
 };
--- Module-8/src/pages/home/HeroSection.tsx
+++ Module-8/src/pages/home/HeroSection.tsx
@@ -1,29 +1,95 @@
 import { Button } from "@/components/ui/button";
 import macbook from "@/assets/images/macbook-exposed.png";
+import { motion } from "framer-motion";
+
+const heroText = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.25,
+      delayChildren: 1,
+    },
+  },
+};
+
+const heroTextChildren = {
+  hidden: {
+    opacity: 0,
+    y: -200,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.7,
+      type: "spring",
+      bounce: 0.5,
+    },
+  },
+};
+
+const heroImage = {
+  initial: { y: 0, rotate: 0, scale: 5 },
+  animate: {
+    y: 20,
+    rotate: -30,
+    scale: 1,
+    transition: {
+      duration: 1,
+      y: {
+        duration: 2,
+        repeat: Infinity,
+        repeatType: "reverse",
+        ease: "easeInOut",
+      },
+    },
+  },
+};
 
 const HeroSection = () => {
   return (
-    <div className="h-screen grid grid-cols-1 lg:grid-cols-2 gap-2 place-items-center justify-items-center py-32">
-      <div className="text-center md:text-left">
-        <h1 className="md:text-7xl lg:text-8xl font-bold">
+    <div className="h-screen grid grid-cols-1 lg:grid-cols-2 gap-2 place-items-center py-32">
+      <motion.div
+        variants={heroText}
+        initial="hidden"
+        animate="visible"
+        // transition={{ duration: 1, type: "tween", staggerChildren:0.5, delayChildren:0.5 }}
+        className="text-center md:text-left"
+      >
+        <motion.h1
+          variants={heroTextChildren}
+          className="md:text-7xl lg:text-8xl font-bold"
+        >
           <span className="text-gray">Don't worry.</span> <br />{" "}
           <span>We'll fix it.</span>
-        </h1>
-        <p className="text-dark-gray text-base md:text-xl max-w-[40ch] my-5">
+        </motion.h1>
+        <motion.p
+          variants={heroTextChildren}
+          className="text-dark-gray text-base md:text-xl max-w-[40ch] my-5"
+        >
           Welcome to <span className="text-black font-semibold">iRepair</span>,
           your one-stop place for all kinds of{" "}
           <span className="text-black font-semibold">Macbook repairs</span> and
           diagnostics.
-        </p>
-        <Button>Book a service</Button>
-      </div>
-      <div className="md:rotate-[-30deg] ">
+        </motion.p>
+        <motion.div variants={heroTextChildren}>
+          <Button>Book a service</Button>
+        </motion.div>
+      </motion.div>
+      <motion.div
+        variants={heroImage}
+        initial="initial"
+        animate="animate"
+        className="md:rotate-[-30deg] "
+      >
         <img
           className="object-contain w-full mx-auto md:w-10/12"
           src={macbook}
           alt=""
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
