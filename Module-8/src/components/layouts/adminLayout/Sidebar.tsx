04db0a00b933dac210d0798776c435bc0d5f4702
Kakon Debnath <kakondebnath96@gmail.com>
2024-01-25 22:20:30 +0600

    fix sidebar err

--- Module-8/src/components/layouts/adminLayout/Sidebar.tsx
+++ Module-8/src/components/layouts/adminLayout/Sidebar.tsx
@@ -1,25 +1,63 @@
+import { cn } from '@/lib/utils';
+import { LayoutDashboard } from 'lucide-react';
 import { NavLink } from 'react-router-dom';
-const navLinks = [
-  { path: '/', name: 'home' },
-  { path: '/admin', name: 'dashboard' },
-  { path: '/admin/add-service', name: 'add service' },
-  { path: '/admin/service-list', name: 'service list' },
-];
 
 const Sidebar = () => {
   return (
     <aside className="bg-light-gray col-span-2 h-screen sticky top-0 left-0 overflow-auto px-3">
       <h2 className="text-center text-3xl py-4">Admin Sidebar</h2>
       <nav className="flex flex-col gap-1">
-        {navLinks.map((navLink, index) => (
-          <NavLink
-            className="px-3 bg-gray py-2 capitalize rounded-md hover:bg-dark-gray hover:text-light-gray hover:font-semibold transition-all duration-300 truncate"
-            key={index}
-            to={navLink?.path}
-          >
-            {navLink?.name}
-          </NavLink>
-        ))}
+        <NavLink
+          className={({ isActive }) =>
+            cn(
+              'px-3 bg-gray py-2 capitalize rounded-md hover:bg-dark-gray hover:text-light-gray hover:font-semibold transition-all duration-300 truncate',
+              { 'bg-red-400': isActive }
+            )
+          }
+          to="/"
+        >
+          <span>Home</span>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            cn(
+              'px-3 bg-gray py-2 capitalize rounded-md transition-all duration-300 flex items-center gap-2',
+              {
+                'bg-dark-gray text-light-gray font-semibold': isActive,
+              }
+            )
+          }
+          to="/admin/dashboard"
+        >
+          <LayoutDashboard className="shrink-0" />
+          <span className="truncate">Dashboard</span>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            cn(
+              'px-3 bg-gray py-2 capitalize rounded-md transition-all duration-300 flex items-center gap-2',
+              {
+                'bg-dark-gray text-light-gray font-semibold': isActive,
+              }
+            )
+          }
+          to="/admin/add-service"
+        >
+          <span>Add Service</span>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            cn(
+              'px-3 bg-gray py-2 capitalize rounded-md transition-all duration-300 flex items-center gap-2',
+              {
+                'bg-dark-gray text-light-gray font-semibold': isActive,
+              }
+            )
+          }
+          to="/admin/service-list"
+        >
+          <span>Service List</span>
+        </NavLink>
       </nav>
     </aside>
   );
